988947398d9e4b2f1a69ea272e890a7461f6e1c6
Sunshine Daydream <alexbruner@live.com>
2018-08-17 21:47:06 -0500

    getting ready to deploy

--- MyPreviousShuffle/src/index.js
+++
@@ -1,9 +0,0 @@
-// Import Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Counter from './components/counterComponent/counter.jsx';
-
-ReactDOM.render(<Counter />, document.getElementById('root'))
